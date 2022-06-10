5901979c0197e385dd6f7e4ba54336c6949a7827
RhyanSPisoni <rhyanoficial14@gmail.com>
2022-06-10 09:29:30 -0300

    Att. Suspender processo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Diagnostics;
+using System;
+using MProcess;
+
+namespace SeassonSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pid = ProcessAccess.GetPidWithName("GTA5");
+            ProcessAccess.SuspendProcess(pid);
+
+            Console.WriteLine($"PID : {pid}");
+            Thread.Sleep(10000);
+
+            ProcessAccess.ResumeProcess(pid);
+        }
+    }
+}
